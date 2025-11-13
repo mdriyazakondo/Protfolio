be59ffc96db5593839637322a8f6b2fcee13ca2b
mdriyazakondo <mdriyazakondo265@gmail.com>
2025-11-14 05:31:56 +0600

    check

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import prerender from "vite-plugin-prerender";
+import tailwindcss from "@tailwindcss/vite";
 
-// Vite config
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(), // React plugin ekbar matro
-    prerender({
-      staticDir: "dist", // build output folder
-      routes: ["/"], // jei pages Google e index korte chan
-      // Example: routes: ["/", "/about", "/projects"]
-    }),
-  ],
-  css: {
-    postcss: {
-      plugins: [
-        require("tailwindcss"), // Tailwind CSS v4.2 plugin
-        require("autoprefixer"),
-      ],
-    },
-  },
+  plugins: [react(), tailwindcss()],
 });
